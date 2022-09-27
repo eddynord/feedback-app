76ac0da2b0a6c4be4396a4735bc4c6be736eef35
Edwin Nord <edwin.nord09@gmail.com>
2022-09-26 23:51:55 -0400

    Feedback UI React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+import FeedbackList from './components/FeedbackList';
 import './App.css';
 
+
+export default App
 function App() {
+const [feedback, setFeedback] = useState(FeedbackData)
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header/>
+    <div className='container'>
+      <FeedbackList
+        feedback = {feedback}
+      />  
+      
     </div>
-  );
+     
+    </>
+    
+      
+      
+    
+
+
+  )
 }
 
-export default App;
+
